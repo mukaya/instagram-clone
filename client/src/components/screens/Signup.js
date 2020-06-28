d2d67e6edc2f1423a7d1e75b52189192bb00ddd6
OseeMukaya <oseemukaya@gmail.com>
2020-06-28 21:37:48 +0200

    make it better

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -69,7 +69,7 @@ const Signup = () => {
   return (
     <div className="mycard">
       <div className="card auth-card input-field input">
-        <h2>Instagram</h2>
+        <h2>osee-sociaux</h2>
         <input
           type="text"
           placeholder="Name"
